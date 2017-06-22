d6391893c9796f4dee6ec5e68595d79add0a4504
Harald Maida <harald.maida@innoave.com>
2017-06-22 19:52:54 +0200

    fixed filename in dep-graph.bat

--- dep-graph.bat
+++ dep-graph.bat
@@ -1,1 +1,1 @@
-cargo graph -I --build-color black --dev-color orange --dev-line-color orange --optional-color purple --optional-line-color purple | dot -Tpng >tsp-graph.png
+cargo graph -I --build-color black --dev-color orange --dev-line-color orange --optional-color purple --optional-line-color purple | dot -Tpng >dep-graph.png
