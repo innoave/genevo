8c7c6ec87cadb2e6a104ff8c52a5fd3bce291612
Harald Maida <harald.maida@innoave.com>
2017-06-24 08:56:38 +0200

    genevo-2 Add default types for binary encoding, value encoding and permutation encoding

---
+++ src/encoding.rs
@@ -0,0 +1,16 @@
+//! The `encoding` module provides basic scheme of encoding `Genotype`s.
+//!
+//! Most important types are:
+//! * binary encoding
+//! * value encoding
+//! * permutation encoding
+//! * tree encoding
+
+use genetic::Genotype;
+
+
+pub struct BitsetEncoding {
+
+    bitset: T,
+
+}
