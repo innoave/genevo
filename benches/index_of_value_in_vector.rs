5d57f658a128157dc166067b774383466574058e
Harald Maida <harald.maida@innoave.com>
2019-06-24 07:18:48 +0200

    use criterion for benchmarking

--- benches/index_of_value_in_vector.rs
+++ benches/index_of_value_in_vector.rs
@@ -1,8 +1,7 @@
-#![feature(test)]
+#[macro_use]
+extern crate criterion;
 
-extern crate test;
-
-use test::Bencher;
+use criterion::Criterion;
 
 #[derive(Debug, PartialEq)]
 struct Gene {
@@ -40,68 +39,103 @@ fn fixture(member: usize, size: usize) -> Fixture {
     }
 }
 
-#[bench]
-fn index_of_gene_in_vector_the_functional_way_200_member(b: &mut Bencher) {
+fn index_of_gene_in_vector_the_functional_way_200_member(c: &mut Criterion) {
     let fixture = fixture(200, 11);
-    b.iter(|| {
-        for i in 0..fixture.data.len() {
-            let gene = &fixture.data[i];
-            fixture.index_of_gene_functional(gene);
-        }
-    })
+    c.bench_function(
+        "index of gene in vector of 200 members the functional way",
+        move |b| {
+            b.iter(|| {
+                for i in 0..fixture.data.len() {
+                    let gene = &fixture.data[i];
+                    fixture.index_of_gene_functional(gene);
+                }
+            })
+        },
+    );
 }
 
-#[bench]
-fn index_of_gene_in_vector_for_loop_over_index_200_member(b: &mut Bencher) {
+fn index_of_gene_in_vector_for_loop_over_index_200_member(c: &mut Criterion) {
     let fixture = fixture(200, 11);
-    b.iter(|| {
-        for i in 0..fixture.data.len() {
-            let gene = &fixture.data[i];
-            fixture.index_of_gene_for_loop_over_index(gene);
-        }
-    })
+    c.bench_function(
+        "index of gene in vector of 200 members using for loop",
+        move |b| {
+            b.iter(|| {
+                for i in 0..fixture.data.len() {
+                    let gene = &fixture.data[i];
+                    fixture.index_of_gene_for_loop_over_index(gene);
+                }
+            })
+        },
+    );
 }
 
-#[bench]
-fn index_of_gene_in_vector_the_functional_way_20_member(b: &mut Bencher) {
+fn index_of_gene_in_vector_the_functional_way_20_member(c: &mut Criterion) {
     let fixture = fixture(20, 200);
-    b.iter(|| {
-        for i in 0..fixture.data.len() {
-            let gene = &fixture.data[i];
-            fixture.index_of_gene_functional(gene);
-        }
-    })
+    c.bench_function(
+        "index of gene in vector of 20 members the functional way",
+        move |b| {
+            b.iter(|| {
+                for i in 0..fixture.data.len() {
+                    let gene = &fixture.data[i];
+                    fixture.index_of_gene_functional(gene);
+                }
+            })
+        },
+    );
 }
 
-#[bench]
-fn index_of_gene_in_vector_for_loop_over_index_20_member(b: &mut Bencher) {
+fn index_of_gene_in_vector_for_loop_over_index_20_member(c: &mut Criterion) {
     let fixture = fixture(20, 200);
-    b.iter(|| {
-        for i in 0..fixture.data.len() {
-            let gene = &fixture.data[i];
-            fixture.index_of_gene_for_loop_over_index(gene);
-        }
-    })
+    c.bench_function(
+        "index of gene in vector of 20 members using for loop",
+        move |b| {
+            b.iter(|| {
+                for i in 0..fixture.data.len() {
+                    let gene = &fixture.data[i];
+                    fixture.index_of_gene_for_loop_over_index(gene);
+                }
+            })
+        },
+    );
 }
 
-#[bench]
-fn index_of_gene_in_vector_the_functional_way_2000_member(b: &mut Bencher) {
+fn index_of_gene_in_vector_the_functional_way_2000_member(c: &mut Criterion) {
     let fixture = fixture(2000, 11);
-    b.iter(|| {
-        for i in 0..fixture.data.len() {
-            let gene = &fixture.data[i];
-            fixture.index_of_gene_functional(gene);
-        }
-    })
+    c.bench_function(
+        "index of gene in vector of 2000 members the functional way",
+        move |b| {
+            b.iter(|| {
+                for i in 0..fixture.data.len() {
+                    let gene = &fixture.data[i];
+                    fixture.index_of_gene_functional(gene);
+                }
+            })
+        },
+    );
 }
 
-#[bench]
-fn index_of_gene_in_vector_for_loop_over_index_2000_member(b: &mut Bencher) {
+fn index_of_gene_in_vector_for_loop_over_index_2000_member(c: &mut Criterion) {
     let fixture = fixture(2000, 11);
-    b.iter(|| {
-        for i in 0..fixture.data.len() {
-            let gene = &fixture.data[i];
-            fixture.index_of_gene_for_loop_over_index(gene);
-        }
-    })
+    c.bench_function(
+        "index of gene in vector of 2000 members using for loop",
+        move |b| {
+            b.iter(|| {
+                for i in 0..fixture.data.len() {
+                    let gene = &fixture.data[i];
+                    fixture.index_of_gene_for_loop_over_index(gene);
+                }
+            })
+        },
+    );
 }
+
+criterion_group!(
+    benches,
+    index_of_gene_in_vector_the_functional_way_200_member,
+    index_of_gene_in_vector_for_loop_over_index_200_member,
+    index_of_gene_in_vector_the_functional_way_20_member,
+    index_of_gene_in_vector_for_loop_over_index_20_member,
+    index_of_gene_in_vector_the_functional_way_2000_member,
+    index_of_gene_in_vector_for_loop_over_index_2000_member,
+);
+criterion_main!(benches);
