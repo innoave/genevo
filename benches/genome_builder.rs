21bce6eb7d18089670af85ca45e810271f171314
haraldmaida <harald.maida@innoave.com>
2021-11-07 08:01:17 +0100

    use benchmark group instead of bench_function_over_inputs

--- benches/genome_builder.rs
+++ benches/genome_builder.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::Criterion;
+use criterion::{BenchmarkId, Criterion};
 use genevo::random::{get_rng, random_seed};
 use rand::{
     distributions::{Bernoulli, Distribution},
@@ -11,23 +11,22 @@ use rand::{
 fn generate_vec_of_random_bool_the_functional_way(c: &mut Criterion) {
     let mut rng = get_rng(random_seed());
 
-    c.bench_function_over_inputs(
-        "generate vec of bool the functional way",
-        move |b, length| {
+    let mut group = c.benchmark_group("generate vec of bool the functional way");
+    for length in [12, 24, 48, 96] {
+        group.bench_with_input(BenchmarkId::from_parameter(length), &length, |b, length| {
             b.iter(|| {
                 let _genome: Vec<bool> = (0..*length).map(|_| rng.gen()).collect();
             })
-        },
-        vec![12, 24, 48, 96],
-    );
+        });
+    }
 }
 
 fn generate_vec_of_random_bool_using_for_loop(c: &mut Criterion) {
     let mut rng = get_rng(random_seed());
 
-    c.bench_function_over_inputs(
-        "generate vec of bool using for loop",
-        move |b, length| {
+    let mut group = c.benchmark_group("generate vec of bool using for loop");
+    for length in [12, 24, 48, 96] {
+        group.bench_with_input(BenchmarkId::from_parameter(length), &length, |b, length| {
             b.iter(|| {
                 let length = *length;
                 let mut genome: Vec<bool> = Vec::with_capacity(length);
@@ -35,33 +34,31 @@ fn generate_vec_of_random_bool_using_for_loop(c: &mut Criterion) {
                     genome.push(rng.gen());
                 }
             })
-        },
-        vec![12, 24, 48, 96],
-    );
+        });
+    }
 }
 
 fn generate_vec_of_random_bool_the_functional_way_with_initialized_distribution(c: &mut Criterion) {
     let bernoulli = Bernoulli::new(0.5).expect("0.5 is a valid probability");
     let mut rng = get_rng(random_seed());
 
-    c.bench_function_over_inputs(
-        "generate vec of bool functional init distribution",
-        move |b, length| {
+    let mut group = c.benchmark_group("generate vec of bool functional init distribution");
+    for length in [12, 24, 48, 96] {
+        group.bench_with_input(BenchmarkId::from_parameter(length), &length, |b, length| {
             b.iter(|| {
                 let _genome: Vec<bool> = (0..*length).map(|_| bernoulli.sample(&mut rng)).collect();
             })
-        },
-        vec![12, 24, 48, 96],
-    );
+        });
+    }
 }
 
 fn generate_vec_of_random_bool_using_for_loop_with_initialized_distribution(c: &mut Criterion) {
     let bernoulli = Bernoulli::new(0.5).expect("0.5 is a valid probability");
     let mut rng = get_rng(random_seed());
 
-    c.bench_function_over_inputs(
-        "generate vec of bool loop init distribution",
-        move |b, length| {
+    let mut group = c.benchmark_group("generate vec of bool loop init distribution");
+    for length in [12, 24, 48, 96] {
+        group.bench_with_input(BenchmarkId::from_parameter(length), &length, |b, length| {
             b.iter(|| {
                 let length = *length;
                 let mut genome: Vec<bool> = Vec::with_capacity(length);
@@ -69,9 +66,8 @@ fn generate_vec_of_random_bool_using_for_loop_with_initialized_distribution(c: &
                     genome.push(bernoulli.sample(&mut rng));
                 }
             })
-        },
-        vec![12, 24, 48, 96],
-    );
+        });
+    }
 }
 
 criterion_group!(
--- benches/population_builder.rs
+++ benches/population_builder.rs
@@ -1,24 +1,27 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::Criterion;
+use criterion::{BenchmarkId, Criterion};
 use genevo::population::{build_population, BinaryEncodedGenomeBuilder};
 
 fn bench_build_population(c: &mut Criterion) {
     let seed = [42; 32];
 
-    c.bench_function_over_inputs(
-        "build population",
-        move |b, size| {
-            b.iter(|| {
-                build_population()
-                    .with_genome_builder::<_, Vec<bool>>(BinaryEncodedGenomeBuilder::new(24))
-                    .of_size(*size)
-                    .using_seed(seed)
-            })
-        },
-        vec![20, 200, 2000, 20_000],
-    );
+    let mut group = c.benchmark_group("build population");
+    for population_size in [20, 200, 2000, 20_000] {
+        group.bench_with_input(
+            BenchmarkId::from_parameter(population_size),
+            &population_size,
+            |b, size| {
+                b.iter(|| {
+                    build_population()
+                        .with_genome_builder::<_, Vec<bool>>(BinaryEncodedGenomeBuilder::new(24))
+                        .of_size(*size)
+                        .using_seed(seed)
+                })
+            },
+        );
+    }
 }
 
 criterion_group!(benches, bench_build_population);
